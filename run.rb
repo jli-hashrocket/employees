f62e1f3b042259e4d7934a61df26d08ec027fee4
macheteshuffle <jeff.li029@gmail.com>
2013-12-04 20:38:24 -0500

    removed pry

--- run.rb
+++ run.rb
@@ -1,5 +1,4 @@
 require 'csv'
-require 'pry'
 
 require_relative 'sales'
 require_relative 'employee_class'
@@ -10,26 +9,3 @@ require_relative 'owner_class'
 Sale.load_sales
 Employee.load_employees
 Employee.print_data
-binding.pry
-
-# CSV.foreach(sales_data, headers:true) do |row|
-#   sale = Sales.new(row)
-#   sales << sale
-#  end
-
-# CSV.foreach(employee_info, headers:true) do |row|
-#    employee =  Employee.new(row,sales)
-#    employees << employee
-# end
-
-
-# employees.each do |employee|
-#   if employee.position != "commission sales"
-#     employee.net_pay_before_tax
-#     employee.net_pay_after_tax
-#     employee.print_data
-#   elsif employee.position == "commission sales"
-#     commission_sales = CommissionSalesPerson.new
-#     commission_sales.net_pay_before_tax
-#   end
-# end
